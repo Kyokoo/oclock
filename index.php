fede829d1237d2f1e74fe93224e8970d7de8a62e
Kyo <genesisrock91@gmail.com>
2017-08-20 20:26:38 +0200

    Mouvements

--- index.php
+++ index.php
@@ -7,22 +7,50 @@
   </head>
   <body style="height:15000px">
     <div id="skrollr-body">
-      <div class="projContainer" data-0="position:fixed" data-1000="position:fixed; width:100vh">
+      <div class="projContainer">
+        <span class="logo" data-0="top:50%; font-size:60pt;" data-top="top:5%;font-size:43pt" data-1000="top:5%;font-size:43pt">bureau __ o'clock</span>
+      </div>
+      <div id="slimi">
         <ul>
-          <li class="projElement bloc"
-          data-center="left:0%;top:50%;"
-          data-top-bottom="left:-40%; top:50%;">
-            <img src="img/portrait01.jpg" class="aligncenter" />
+          <li class="simpleBlock portrait">
           </li>
-          <li class="projElement bloc"
-          data-center="left:0%;top:50%;"
-          data-top-bottom="left:-40%; top:50%;"
-          >
-            <img src="img/portrait02.jpg" class="aligncenter" />
+          <li class="simpleBlock portrait">
           </li>
         </ul>
       </div>
-	  </div>
+      <div class="projContainer" data-0="display:none;" data-1500="display:block;">
+        <div class="projElement landscape" data-1500="left:-25%" data-2500="left:100%" style="top:40%;"></div>
+      </div>
+
+      <div class="projContainer" data-0="display:none;" data-2200="display:block;">
+        <div class="projElement landscape" data-2200="left:100%" data-3200="left:-25%" style="top:60%;"></div>
+      </div>
+
+      <div class="landscape block" style="top:3500px; left:25%;">
+
+      </div>
+      <div class="landscape block" style="top:4400px; left:55%;">
+
+      </div>
+
+      <div class="projContainer" data-0="display:none;" data-3000="display:block;">
+        <div class="projElement landscape" data-5400="top:-20%" data-7500="top:120%" style="left:50%;"></div>
+      </div>
+
+      <div class="landscape block" style="top:5200px; left:35%;">
+
+      </div>
+
+      <div class="projContainer" data-0="display:none;" data-7500="display:block;">
+        <div class="projElement landscape" data-7500="left:-25%" data-8500="left:100%" style="top:40%;"></div>
+      </div>
+
+      <div class="landscape block" style="top:6200px; left:35%;">
+
+      </div>
+
+
+    </div>
     <script type="text/javascript" src="js/skrollr.min.js"></script>
     <script type="text/javascript">
       var s = skrollr.init();
