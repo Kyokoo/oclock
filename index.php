4d1b9fa713882af65094ecbf18a6724095850810
Kyo <genesisrock91@gmail.com>
2017-08-20 12:26:07 +0200

    Project setup

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="skrollr skrollr-desktop">
+  <head>
+    <meta charset="utf-8">
+    <title>Bureau O'Clock</title>
+    <link rel="stylesheet" href="css/style.css"/>
+  </head>
+  <body style="height:15000px">
+    <div id="skrollr-body">
+      <div class="projContainer" data-0="position:fixed" data-1000="position:fixed; width:100vh">
+        <ul>
+          <li class="projElement bloc"
+          data-center="left:0%;top:50%;"
+          data-top-bottom="left:-40%; top:50%;">
+            <img src="img/portrait01.jpg" class="aligncenter" />
+          </li>
+          <li class="projElement bloc"
+          data-center="left:0%;top:50%;"
+          data-top-bottom="left:-40%; top:50%;"
+          >
+            <img src="img/portrait02.jpg" class="aligncenter" />
+          </li>
+        </ul>
+      </div>
+	  </div>
+    <script type="text/javascript" src="js/skrollr.min.js"></script>
+    <script type="text/javascript">
+      var s = skrollr.init();
+	  </script>
+  </body>
+</html>
