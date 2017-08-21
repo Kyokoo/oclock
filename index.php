ac607570f4008dde3854a891d7b4746a433a1617
Kyo <genesisrock91@gmail.com>
2017-08-21 20:19:09 +0200

    Opacity control on logo

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
       <div class="projContainer">
         <div id="logo" data-0="top:50%;" data-1000="top:5%;">
           <span class="logo alignLogo" >bureau</span>
-          <span class="logo underline">&nbsp;4&nbsp;</span>
+          <span class="logo underline" data-0="color:rgba(0,0,0,0)" data-700="color:rgba(0,0,0,0)" data-1000="color:rgba(0,0,0,1)">&nbsp;4&nbsp;</span>
           <span class="logo alignLogo">o'clock</span>
         </div>
       </div>
@@ -30,11 +30,11 @@
         <div class="caption">Block02</div>
       </div>
       <div class="projContainer" data-0="display:none;" data-1500="display:block;">
-        <div class="projElement landscape" data-1500="left:-25%" data-2500="left:100%" style="top:40%;"></div>
+        <div class="projElement landscape" data-1500="left:-25%" data-2500="left:100%" style="top:50%;"></div>
       </div>
 
       <div class="projContainer" data-0="display:none;" data-2200="display:block;">
-        <div class="projElement landscape" data-2200="left:100%" data-3200="left:-25%" style="top:60%;"></div>
+        <div class="projElement landscape" data-2200="left:100%" data-3200="left:-25%" style="top:50%;"></div>
       </div>
 
       <div class="landscape block" style="top:3500px; left:25%;">
