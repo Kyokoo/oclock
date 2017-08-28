c877804f1e0134a4902b3ff96fc26667ecb562cf
Kyo <genesisrock91@gmail.com>
2017-08-28 19:28:24 +0200

    Hover images animations fix

--- index.php
+++ index.php
@@ -28,50 +28,50 @@
       </div>
 
       <div id="project01">
-          <div id="project01-1" class="simpleBlock portrait simpleBlockLeft blockhover"><img src="img/portrait01.jpg" /></div>
+          <div id="project01-1" class="simpleBlock portrait simpleBlockLeft blockhover"><figure><img src="img/portrait01.jpg" /></figure></div>
           <div id="captionProject01-1" class="caption" data-captionLeftStart="5" data-captionLeftDist="20" data-captionTopStart="15" data-captionTopDist="50">
             <h3>Slimi Magazine</h3> <br/>Fashion magazine<br/>& website</div>
-          <div id="project01-2" class="simpleBlock portrait simpleBlockRight blockhover"><img src="img/portrait02.jpg" /></div>
+          <div id="project01-2" class="simpleBlock portrait simpleBlockRight blockhover"><figure><img src="img/portrait02.jpg" /></figure></div>
           <div id="captionProject01-2" class="caption" data-captionLeftStart="70" data-captionLeftDist="20" data-captionTopStart="15" data-captionTopDist="50">
             <h3>Block02</h3> <br/> BlaBla BlaBla <br/> BlaBla BlaBla</div>
       </div>
       <div id="project02" class="projContainer" data-0="display:none;" data-1500="display:block;">
-        <div id="project02-1" class="projElement landscape blockhover" data-1500="left:-25%" data-2500="left:100%" style="top:50%;"></div>
+        <div id="project02-1" class="projElement landscape blockhover" data-1500="left:-25%" data-2500="left:100%" style="top:50%;"><figure><img src="img/portrait01.jpg" /></div>
         <div id="captionProject02-1" class="caption" data-captionLeftStart="70" data-captionLeftDist="20" data-captionTopStart="15" data-captionTopDist="50">
           <h3>Block03</h3> <br/> BlaBla BlaBla <br/> BlaBla BlaBla</div>
       </div>
 
       <div id="project03" class="projContainer" data-0="display:none;" data-2200="display:block;">
-        <div id="project03-1" class="projElement landscape blockhover" data-2200="left:100%" data-3200="left:-25%" style="top:50%;"></div>
+        <div id="project03-1" class="projElement landscape blockhover" data-2200="left:100%" data-3200="left:-25%" style="top:50%;"><figure><img src="img/portrait01.jpg" /></div>
         <div id="captionProject03-1" class="caption" data-captionLeftStart="10" data-captionLeftDist="20" data-captionTopStart="15" data-captionTopDist="50">
           <h3>Block04</h3> <br/> BlaBla BlaBla <br/> BlaBla BlaBla</div>
       </div>
 
       <div id="project04">
-        <div id="project04-1" class="landscape block blockhover" style="top:3500px; left:25%;"></div>
+        <div id="project04-1" class="landscape block blockhover" style="top:3500px; left:25%;"><figure><img src="img/portrait01.jpg" /></div>
         <div id="captionProject04-1" class="caption" data-captionLeftStart="60" data-captionLeftDist="30" data-captionTopStart="50" data-captionTopDist="30">
           <h3>Block05</h3> <br/> BlaBla BlaBla <br/> BlaBla BlaBla</div>
       </div>
       <div id="project05">
-        <div id="project05-1" class="landscape block blockhover" style="top:4400px; left:55%;"></div>
+        <div id="project05-1" class="landscape block blockhover" style="top:4400px; left:55%;"><figure><img src="img/portrait01.jpg" /></div>
         <div id="captionProject05-1" class="caption" data-captionLeftStart="10" data-captionLeftDist="40" data-captionTopStart="20" data-captionTopDist="50">
           <h3>Block06</h3> <br/> BlaBla BlaBla <br/> BlaBla BlaBla</div>
       </div>
 
       <div class="projContainer" data-0="display:none;" data-3000="display:block;">
-        <div class="projElement landscape blockhover" data-5400="top:-20%" data-7500="top:120%" style="left:50%;"></div>
+        <div class="projElement landscape blockhover" data-5400="top:-20%" data-7500="top:120%" style="left:50%;"><figure><img src="img/portrait01.jpg" /></div>
       </div>
 
       <div class="landscape block blockhover" style="top:5200px; left:35%;">
-
+<figure><img src="img/portrait01.jpg" />
       </div>
 
       <div class="projContainer" data-0="display:none;" data-7500="display:block;">
-        <div class="projElement landscape blockhover" data-7500="left:-25%" data-8500="left:100%" style="top:40%;"></div>
+        <div class="projElement landscape blockhover" data-7500="left:-25%" data-8500="left:100%" style="top:40%;"><figure><img src="img/portrait01.jpg" /></div>
       </div>
 
       <div class="landscape block blockhover" style="top:6200px; left:35%;">
-
+<figure><img src="img/portrait01.jpg" />
       </div>
 
 
